e1d27deaddcdeff77caad6ffd44f17a348ddcbb1
vitor José <vitorjsls30@gmail.com>
2021-03-02 21:52:57 -0300

    nlw4-01 - Adding First Component ExperienceBar

---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,16 @@
+import { isJSDocReturnTag, isReturnStatement } from "typescript";
+
+export default function ExperienceBar() {
+    return(
+        <header className="experience-bar">
+            <span>0 xp</span>
+            <div>
+                <div style={{ width: '50%' }}></div>
+                <span className="current-experience" style={{ left: '50%' }}>
+                    300 xp
+                </span>
+            </div>
+            <span>600 xp</span>
+        </header>
+    );
+}
